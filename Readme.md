f1834b7f9c61d1f5d671ffea7c84c7021a75bc06
ajithvallabai <inocajith21.5@gmail.com>
2022-10-20 09:52:10 +0530

    Add script, Readme, requirements files

---
+++ Readme.md
@@ -0,0 +1,17 @@
+### Meaning Finder
+
+**Prerequesties**
+- PyDictionary
+- Internet connection
+
+**Usage**
+- python3 main.py
+
+**Explanation**
+- Write your words in each line without spelling mistakes in words.txt
+- Using internet connection Pydictionary fetches content and gives you the meaning
+- You would get your output in output.txt file
+- Usefull while finding meaning for 1000's of words
+
+**Visuval**
+![Visuval Sample](./sample.png)
---
+++ main.py
@@ -0,0 +1,32 @@
+from PyDictionary import PyDictionary
+
+if __name__ == "__main__":
+
+    # Call PyDictionary class
+    dc = PyDictionary()
+
+    # Opening input file
+    # read from a file line by line
+    # Get meaning of each word
+    wordsFile = open('words.txt', 'r')
+    count = 0
+    wordMeanCollector = {}
+    # Using for loop
+    for line in wordsFile:
+        count += 1
+        print("Line{}: {}".format(count, line.strip()))
+        meaning = dc.meaning(line.strip())
+        wordMeanCollector[count] = [line.strip(),meaning]
+    # Closing file
+    wordsFile.close()
+
+    # open output file
+    outputFile = open('output.txt', 'w')
+    # Structure and write to file
+    for each in wordMeanCollector:
+        subjectDict = list(wordMeanCollector[each][1].keys())
+        subjectList = [''.join(ele) for ele in subjectDict]
+        MeaningDict = list(wordMeanCollector[each][1].values())
+        MeaningList = [','.join(ele) for ele in MeaningDict]
+        outputFile.write("%s - %s - %s \n" % (wordMeanCollector[each][0], ",".join(subjectList), MeaningList[0]))
+    outputFile.close()
